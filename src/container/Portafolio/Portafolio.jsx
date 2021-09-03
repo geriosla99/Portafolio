5684515cfd75999ef73e9c687e5999ae27a0970f
geriosla99 <geriosla@gmail.com>
2021-09-02 23:25:34 -0500

    clean code of things that i dont use

--- src/container/Portafolio/Portafolio.jsx
+++ src/container/Portafolio/Portafolio.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import About from '../../components/About/About'
-import Form from '../../components/Form/Form'
-import Header from "../../components/Header/Header";
 
 const Portafolio = () => {
     return (
