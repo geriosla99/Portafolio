966df364e155282c3c42d16d82ccadbe8bf6178f
geriosla99 <geriosla@gmail.com>
2021-09-02 23:12:26 -0500

    add more thigs

--- src/container/Portafolio/Portafolio.jsx
+++ src/container/Portafolio/Portafolio.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import About from '../../components/About/About'
+import Form from '../../components/Form/Form'
+import Header from "../../components/Header/Header";
 
 const Portafolio = () => {
     return (
         <>
-            
+            <Header/>
+            <About/>
+            <Form/>
         </>
     )
 }
