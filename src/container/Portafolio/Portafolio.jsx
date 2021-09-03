afd3f0667e583f0e3c61aef2bde86ed82b6c9587
geriosla99 <geriosla@gmail.com>
2021-09-02 23:03:34 -0500

    clean

--- src/container/Portafolio/Portafolio.jsx
+++ src/container/Portafolio/Portafolio.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import About from '../../components/About/About'
-import { ContainerEjemplo, ContenedorEjemplo } from '../../components/About/AboutStyled'
 
 const Portafolio = () => {
     return (
