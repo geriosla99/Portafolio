ae109316191b8cc35aff22b0d276d1d79de08fee
geriosla99 <geriosla@gmail.com>
2021-10-17 16:18:42 -0500

    add Styles to my profile photo

--- src/components/About/AboutStyled.js
+++ src/components/About/AboutStyled.js
@@ -2,15 +2,16 @@ import styled from 'styled-components'
 
 export const ContainerPhoto = styled.div`
     width: 15vw;  
-    margin-top: -3em;  
-    
+    margin-top: -3em; 
+    margin-left: 4em;
+    position: absolute;
 `;
 
-export const ProfilePhoto = styled.img`
+export const ProfilePhoto = styled.img`    
     width: 100%;
     height: auto;
     border-radius: 100%;
     box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.52);
--webkit-box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.52);
--moz-box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.52);
+    -webkit-box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.52);
+    -moz-box-shadow: 0px 0px 8px 0px rgba(0,0,0,0.52);
 `;
