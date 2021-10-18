254860d711dd7417adcad637fdcbe29d52f2279c
geriosla99 <geriosla@gmail.com>
2021-10-17 21:32:08 -0500

    make my info

--- src/components/MyInfo/MyInfo.jsx
+++ src/components/MyInfo/MyInfo.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Resumen } from './MyInfoStyled'
+import { ContentResume, Resumen, ResumenText } from './MyInfoStyled'
 
 const MyInfo = () => {
     return (
         <>
-            <Resumen>
-                <p>
-                Soy una persona proactiva con motivación al logro, alta empatía y disposición a trabajar en equipos de alto desempeño, preparada para trabajar bajo presión, manejo de ReactJS, dominio intermedio de JavaScript, dominio de HTML y CSS, dominio de librerías de estilo como Bootstrap y conocimientos de la librería Chakra, conocimientos en Figma, Marvel app, Balsamiq y uso intermedio de Canva, conocimientos básicos de hardware, manejo de sistema operativo Windows y suite de ofimática office. Mis pasatiempos favoritos dibujar, diseñar, me apasiona aprender cosas nuevas y las nuevas vivencias.
-                </p>
-            </Resumen>
-            
+            <ContentResume>
+                <Resumen>
+                    <ResumenText>
+                        Soy una persona proactiva con motivación al logro, alta empatía y disposición a trabajar en equipos de alto desempeño, preparada para trabajar bajo presión, manejo de ReactJS, dominio intermedio de JavaScript, dominio de HTML y CSS, dominio de librerías de estilo como Bootstrap y conocimientos de la librería Chakra, conocimientos en Figma, Marvel app, Balsamiq y uso intermedio de Canva, conocimientos básicos de hardware, manejo de sistema operativo Windows y suite de ofimática office. Mis pasatiempos favoritos dibujar, diseñar, me apasiona aprender cosas nuevas y las nuevas vivencias.
+                    </ResumenText>
+                </Resumen>
+            </ContentResume>                        
         </>
     )
 }
--- src/components/MyInfo/MyInfoStyled.js
+++ src/components/MyInfo/MyInfoStyled.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components'
 
-export const Resumen = styled.div`
-    width: 80%;
+export const ContentResume = styled.div`
+    width: 100%;
     display: flex;
     justify-content: center;
+    margin-top: 3em;
+`;
+
+export const Resumen = styled.span`
+    width: 80%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    
+`;
+
+export const ResumenText = styled.p`
+    text-align: justify;
 `;
