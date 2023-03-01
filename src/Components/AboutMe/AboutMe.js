232a0607dec49e9c89302fed65e0ac2212ecd2b1
Geraldine <geriosla@gmail.com>
2023-02-28 20:12:25 -0500

    adding responsive

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -15,27 +15,58 @@ export const Content = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    @media only screen and (max-width:425px){
+        flex-direction: column;
+    }
 `;
 
-export const Name = styled.h1`
+export const Name = styled.p`
     font-size: 9rem;
+    font-weight: 700;
     width: 5rem;
+    @media only screen and (max-width:1200px){
+        font-size: 7rem !important;
+    }
+    @media only screen and (max-width:1024px){
+        font-size: 5rem !important;
+    }
+    @media only screen and (max-width:768px){
+        font-size: 3rem !important;
+    }
+    @media only screen and (max-width:320px){
+        font-size: 1.5rem !important;
+    }
 `;
 
 export const PhotoGRL = styled.img`
     width: 20rem;
     height: auto;
     border-radius: 2%;
+    @media only screen and (max-width:425px){
+        width: 10rem;
+    }
+    @media only screen and (max-width:320px){
+        width: 5rem;
+    }
 `;
 export const Profesion = styled.p`
     font-size: 2rem;
+    @media only screen and (max-width:320px){
+        font-size: 1rem !important;
+    }
 `;
 
 export const InfoChange = styled.div`
     width: 100%;
 `;
 
-export const Description = styled.h2`
+export const Description = styled.p`
     text-align: justify;
     padding: 2rem;
+    font-size: 9rem;
+    
+    @media only screen and (max-width:320px){
+        font-size: 1.5rem !important;
+    }
+    
 `;
