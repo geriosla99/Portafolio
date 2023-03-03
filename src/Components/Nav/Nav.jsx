3d58d65d07813f6886217884a4520a8ec4c7ea5c
Geraldine <geriosla@gmail.com>
2023-03-03 13:44:00 -0500

    menu responsive

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import './Nav.css'
+import { useRef } from "react";
 import { Header, Li, Link, Navigator, Ul } from './NavStyled'
+import { FaTimes, FaBars } from "react-icons/fa";
 
 const Nav = () => {
+
+  const navRef= useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  }
+
   return (
     <div>
         <Header>
-            <Navigator>
+            <Navigator ref={navRef}>
                 <Ul>
                     <Li className='list-nav'><Link  className='link-nav' href="#">Inicio</Link></Li>
                     <Li className='list-nav'><Link  className='link-nav' href="#">Portafolio</Link></Li>
                     <Li className='list-nav'><Link  className='link-nav' href="#">Blog</Link></Li>
                     <Li className='list-nav'><Link  className='link-nav' href="#">Contacto</Link></Li>
+                    <button className='nav-btn' onClick={showNavbar}>
+                        <FaTimes/>
+                    </button>
                 </Ul>
             </Navigator>
+            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+              <FaBars/>
+            </button>
         </Header>
     </div>
   )
