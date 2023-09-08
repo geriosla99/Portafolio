022da0ebbaf94adbfc92d405a51ca325b8e703fb
Geraldine <geriosla@gmail.com>
2023-09-07 20:20:02 -0500

    ajustes

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -9,19 +9,30 @@ const AboutMe = () => {
     <div>
       <Container>
         <Content>
-        <InfoChange onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>
-    {mostrarComponente ? (
-      <>
-        <Name className="show-element">Geraldine Rios</Name>
-        <Profesion className="show-element">Desarrolladora Web</Profesion>
-      </>
-    ) : (
-      <Description>Soy la mejor opción porque poseo una amplia experiencia en el diseño y desarrollo de sitios web, además de contar con habilidades en programación y resolución de problemas. Soy apasionada por mi trabajo y estoy comprometida en ofrecer soluciones eficientes y de alta calidad a mis clientes. </Description>
-    )}
-  </InfoChange>
+          <InfoChange onMouseEnter={handleMouseEnterLeave} onMouseLeave={handleMouseEnterLeave}>
+            {mostrarComponente ? (
+              <>
+                <Name className="show-element">Geraldine Rios</Name>
+                <Profesion className="show-element">Desarrolladora Web</Profesion>
+              </>
+            ) : (
+              <Description>Experta en diseño y desarrollo de sitios web con una sólida experiencia en programación y resolución de problemas. Comprometida con ofrecer soluciones eficientes y de alta calidad. ¡Contáctame para llevar tu presencia en línea al siguiente nivel!</Description>
+            )}
+          </InfoChange>
           <PhotoGRL src="https://i.imgur.com/maRpDEd.jpg" alt="" />
         </Content>
       </Container>
+      <p>
+        Si estás buscando servicios de diseño y desarrollo de sitios web, has llegado al lugar adecuado. En mi extensa trayectoria, he acumulado una sólida experiencia en la creación de sitios web que no solo son visualmente atractivos, sino que también funcionan de manera eficiente.
+
+        Como experta en programación y solución de problemas, estoy comprometida en ofrecer soluciones de alta calidad que se adapten a tus necesidades específicas. Mi pasión por este trabajo se refleja en cada proyecto que emprendo.
+
+        Mis servicios abarcan desde la conceptualización y diseño inicial hasta la implementación y el mantenimiento continuo. Estoy aquí para ayudarte a alcanzar tus objetivos en línea, ya sea que necesites un sitio web personal, una tienda en línea o una plataforma empresarial compleja.
+
+        En cada etapa del proceso, me esfuerzo por superar las expectativas de mis clientes, proporcionando resultados que no solo impresionan, sino que también impulsan el éxito en línea.
+
+        Si valoras la experiencia, la calidad y el compromiso, no dudes en ponerte en contacto conmigo. Juntos, podemos llevar tu presencia en línea al siguiente nivel.
+      </p>
     </div>
   )
 }
