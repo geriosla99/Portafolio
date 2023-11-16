68f56398335e3897fabd445bfe268c7183ae5047
Geraldine <geriosla@gmail.com>
2023-11-16 00:48:33 -0500

    cambio de colores y agregar burbujas

--- src/Components/Nav/NavStyled.js
+++ src/Components/Nav/NavStyled.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const Header = styled.header`
-    background: #ffffff;
     width: 100%;
     position: fixed;
     z-index: 100;
     display: flex;
     justify-content: space-around;
+    margin-top: 0.5rem;
+    backgroud: #FFFFFF5; 
 `;
 
 export const Navigator = styled.nav`
@@ -18,17 +19,21 @@ export const Ul = styled.ul`
 `;
 
 export const Li = styled.li`
+    background: #ffffff;
     float: left;    
     font-size: 20px; 
+    border-radius: 1rem;
+    margin: 0 2rem;
 `;
 
 export const Link = styled.a`
     display: block; 
-    padding: 20px;
+    padding: 10px;
     color: #9d8189;
     text-decoration: none;
 `;
 
 export const Logo = styled.img`
     width: 5rem;
+    border-radius: 1rem;
 `;
